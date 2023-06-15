7bf996dc731f0bddd098c52e5b17ad34089d3add
Taylor McKay <jtmckay@gmail.com>
2023-06-15 11:07:50 -0600

    Fix types (unpublished)

--- packages/persist/index.ts
+++ packages/persist/index.ts
@@ -1,37 +1,37 @@
-import { v4 as uuid } from 'uuid'
-import { concatMap, map } from "rxjs"
+import { concatMap, map } from 'rxjs';
+import { v4 as uuid } from 'uuid';
 
 export type Persistable = {
-  id: string
-  created_at: number
-}
+  id: string;
+};
 
 export type Persistence<Entity, PersistenceEntityType> = {
-  indexedDB?: any
-  put: (entity: PersistenceEntityType, key: string | { id: string }, value: any) => Promise<Entity & Persistable>
-  get: (entity: PersistenceEntityType, key: string | { id: string }) => Promise<Entity & Persistable | undefined>
-  query: (entity: PersistenceEntityType, key?: string | { id: string }) => Promise<Entity & Persistable[]>
-  remove: (entity: PersistenceEntityType, key: string | { id: string }) => Promise<Entity & Persistable>
-}
+  get: (entity: PersistenceEntityType, key: string | { id: string }) => Promise<(Entity & Persistable) | undefined>;
+  indexedDB?: any;
+  put: (entity: PersistenceEntityType, key: string | { id: string }, value: any) => Promise<Entity & Persistable>;
+  query: (entity: PersistenceEntityType, key?: string | { id: string }) => Promise<Entity & Persistable[]>;
+  remove: (entity: PersistenceEntityType, key: string | { id: string }) => Promise<Entity & Persistable>;
+};
 
-export function toPersistable (value) {
+export function toPersistable<T>(value: T & { id?: string }): T & { id: string } {
   if (!value.id) {
-    value.id = uuid()
+    value.id = uuid();
   }
-  if (!value.created_at) {
-    value.created_at = Date.now()
-  }
-  return value
+  return value as T & { id: string };
 }
 
-export function mapToPersistable<T> () {
-  return map<T, T & Persistable>(toPersistable)
+export function mapToPersistable<T>() {
+  return map<T & { id?: string }, T & Persistable>(toPersistable);
 }
 
-export function concatMapPersist<Entity, PersistenceEntityType> (entity: PersistenceEntityType) {
-  return concatMap<[Persistable, Persistence<Entity, PersistenceEntityType>], any>(([value, persistence]) => persistence.put(entity, { id: value.id }, value))
+export function concatMapPersist<Entity, PersistenceEntityType>(entity: PersistenceEntityType) {
+  return concatMap<[Persistable, Persistence<Entity, PersistenceEntityType>], any>(([value, persistence]) =>
+    persistence.put(entity, { id: value.id }, value)
+  );
 }
 
-export function concatMapRemove<Entity, PersistenceEntityType> (entity: PersistenceEntityType) {
-  return concatMap<[Persistable, Persistence<Entity, PersistenceEntityType>], any>(([value, persistence]) => persistence.remove(entity, value))
+export function concatMapRemove<Entity, PersistenceEntityType>(entity: PersistenceEntityType) {
+  return concatMap<[Persistable, Persistence<Entity, PersistenceEntityType>], any>(([value, persistence]) =>
+    persistence.remove(entity, value)
+  );
 }
